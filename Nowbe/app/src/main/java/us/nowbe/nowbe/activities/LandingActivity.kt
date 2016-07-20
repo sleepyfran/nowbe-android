bdae19a0d8888b047c1f13ead15bcd8fbd3b200a
Fran González <zox198@gmail.com>
2016-07-20 14:36:39 +0200

    Removed unused dependencies + added onPageChangeListener

--- Nowbe/app/src/main/java/us/nowbe/nowbe/activities/LandingActivity.kt
+++ Nowbe/app/src/main/java/us/nowbe/nowbe/activities/LandingActivity.kt
@@ -8,8 +8,9 @@ package us.nowbe.nowbe.activities
  */
 
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_base_no_tabs.*
+import android.support.v4.view.ViewPager
 import kotlinx.android.synthetic.main.activity_base_tabs.*
+import us.nowbe.nowbe.R
 import us.nowbe.nowbe.utils.TabUtils
 
 class LandingActivity : BaseActivity() {
@@ -18,10 +19,32 @@ class LandingActivity : BaseActivity() {
         super.onCreate(savedInstanceState)
 
         // Setup the view pager and the tab layout
-        vpFragmentList.adapter = TabUtils.createPagerAdapter(this, supportFragmentManager)
+        val adapter = TabUtils.createPagerAdapter(this, supportFragmentManager)
+        vpFragmentList.adapter = adapter
         tlTabs.setupWithViewPager(vpFragmentList)
 
-        // TODO: Provide a method to dynamically change the menu bar
+        // TODO: Update the menu of the activity when navigating through the ViewPager
+        vpFragmentList.addOnPageChangeListener(object : ViewPager.OnPageChangeListener {
+            override fun onPageSelected(position: Int) {
+                val pageTitle = adapter.getPageTitle(position)
+
+                if (pageTitle == getString(R.string.main_feed_tab)) {
+                    // TODO: Show the edit fab
+                } else if (pageTitle == getString(R.string.main_notifications_tab)) {
+                    // TODO: Show the clear all fab
+                } else if (pageTitle == getString(R.string.main_profile_tab)) {
+                    // TODO: Think what the fuck to show in here
+                }
+            }
+
+            override fun onPageScrollStateChanged(state: Int) {
+                // Nothing
+            }
+
+            override fun onPageScrolled(position: Int, positionOffset: Float, positionOffsetPixels: Int) {
+                // Nothing
+            }
+        })
     }
 
     override fun hasTabs(): Boolean {
